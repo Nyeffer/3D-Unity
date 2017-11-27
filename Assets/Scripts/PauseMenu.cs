487352b569c54f340463008278074c8dcc754c7c
Jaype27 <jaypee.0927@gmail.com>
2017-11-26 23:03:50 -0500

    pause menu & script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject gamePause;
+
+	public void Pause () {
+			Time.timeScale = 0f;
+			gamePause.SetActive(true);
+	}
+
+	public void Resume () {
+		Time.timeScale = 1f;
+		gamePause.SetActive(false);
+	}
+	
+	public void Quit () {
+		Application.Quit();
+	}
+}
