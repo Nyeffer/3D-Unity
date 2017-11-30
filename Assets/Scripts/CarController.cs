74788f44d696771aef8680dfb25c16a29c090e73
Jeffherson Vega <jeffhersonlim@gmail.com>
2017-11-29 20:53:21 -0500

    Cars In

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public float m_moveSpeed;
+
+	void Start() {
+		m_moveSpeed = 25.0f;
+	}
+
+
+	void Update() {
+		Vector3 pos = gameObject.transform.position;
+		pos.z = pos.z + m_moveSpeed * Time.deltaTime;
+		gameObject.transform.position = pos;
+	}
+}
