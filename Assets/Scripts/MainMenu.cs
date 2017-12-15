ca89609c407372dd3ab1253d083103887e24de29
Jaype27 <jaypee.0927@gmail.com>
2017-12-15 18:29:49 -0500

    changed box col on the cars

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string m_LeveltoLoad;
+	
+	public void PlayGame () {
+		SceneManager.LoadScene(m_LeveltoLoad);
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+}
+
